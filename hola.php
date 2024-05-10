479eb3d6b01e25a2ed1ebfe426bb614528270b8e
“Jorteva042“ <tartessoshu1981@hotmail.com>
2024-05-10 20:33:19 +0200

    Primer commit

---
+++ hola.php
@@ -0,0 +1,9 @@
+<?php
+/*José Manuel Ortiz Evangelista*/
+//Autor: Jorteva042 <jorteva042@hola.com>
+require('holamundo.php');
+
+print "introduce tu nombre"
+$nombre = trim(fgets(STDIN));
+print "Hola Soy {$nombre}\n";
+?>
